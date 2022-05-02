a1ea528193434c623ddce06abb073a7d7aaa4628
raptor0 <xarr0w305@gmail.com>
2022-05-03 01:07:51 +0300

    Update MainScript.lua

--- MainScript.lua
+++ MainScript.lua
@@ -14,7 +14,7 @@ end
 local discordinv = "https://discord.gg/tgh3dtSEW7"
 local d
 local f = pcall(function()
-    d = game:HttpGet("https://raw.githubusercontent.com/Discord0000/Zer0Hub/main/GameScripts/"..game.PlaceId..".lua")
+    d = game:HttpGet("https://raw.githubusercontent.com/Discord0000/Zer0Hub/main/GameScripts/"..game..".lua")
 end)
 if f == true then
     loadstring(d)()
